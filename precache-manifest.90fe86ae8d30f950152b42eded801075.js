d7e42a2ad05c1549c45a0f5e6a2a31b35712d75e
HrridoyV2 <67540971+HrridoyV2@users.noreply.github.com>
2020-08-25 14:48:20 +0600

    V

---
+++ precache-manifest.90fe86ae8d30f950152b42eded801075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fb5dc20b968e6e642f554dc666a841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2015a73f1a041381f7db",
+    "url": "/static/css/main.89c4b6fa.chunk.css"
+  },
+  {
+    "revision": "8c19ded9dfdd9ab96fe6",
+    "url": "/static/js/2.801116bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.801116bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2015a73f1a041381f7db",
+    "url": "/static/js/main.7deeba13.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/static/media/logo.95f238a5.png"
+  }
+]);
